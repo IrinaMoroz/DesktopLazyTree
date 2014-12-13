4e9c86a9ac8bfaf3ea80494d4e26ff44d4bf2078
IrinaMoroz <irinamvic@gmail.com>
2014-12-13 17:48:03 +0200

    make up

--- DesktopLazyTree/MainWindow.xaml.cs
+++ DesktopLazyTree/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace DesktopLazyTree
         {
             InitializeComponent();
         }
-
+        
         private void TreeView_Loaded(object sender, RoutedEventArgs e)
         {
             DriveInfo[] allDrives = DriveInfo.GetDrives();
@@ -35,14 +35,7 @@ namespace DesktopLazyTree
             foreach (DriveInfo d in allDrives)
             {
                 if (d.DriveType != DriveType.Unknown && d.IsReady )
-                {
-                    TreeViewItem tw = new TreeViewItem();
-                    Element el = new Element(d.Name, true);
-                    tw.Header = el;
-                    tw.Items.Add("");
-                    roots.Add(tw);
-                }
-
+                    roots.Add(CreateChild(d.Name, true));
             }
 
             var tree = sender as TreeView;
@@ -79,43 +72,48 @@ namespace DesktopLazyTree
         }
 
         private void SetSubs(TreeViewItem root)
-        {
-            
-            string[] files, subDirs;
-           
-            if (root != null)
-            {                
-                try
-                {
-                    subDirs = System.IO.Directory.GetDirectories(((Element)root.Header).Path);
-                    files = System.IO.Directory.GetFiles(((Element)root.Header).Path);
-                    foreach (var dir in subDirs)
-                    {
-                        root.Items.Remove("");
-                        TreeViewItem child = new TreeViewItem();
-                        Element el = new Element(dir, true);
-                        child.Header = el;
-                        child.Items.Add("");
-                        root.Items.Add(child);
-                    }
-
-                    foreach (var fl in files)
-                    {
-                        Element el = new Element(fl, false);
-                        TreeViewItem child = new TreeViewItem();
-                        child.Header = el;
-                        root.Items.Add(child);
-                    }
-                        
-                    
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    MessageBox.Show(String.Format("You have no Access to {0}", ((Element)root.Header).Path));
-                }
+        {                            
+            try
+            {
+                SetSubDirs(root);
+                SetSubFolders(root);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show(String.Format("You have no access to {0}", ((Element)root.Header).Path));
             }
         }
 
-    }
 
+        private void SetSubDirs(TreeViewItem root)
+        {
+          
+            string[] subDirs = System.IO.Directory.GetDirectories(((Element)root.Header).Path);
+          
+            foreach (var dir in subDirs)
+            {
+                root.Items.Remove("");
+                root.Items.Add(CreateChild(dir, true));
+            } 
+            
+        }
+
+        private void SetSubFolders(TreeViewItem root)
+        {
+          string[] files = System.IO.Directory.GetFiles(((Element)root.Header).Path);
+
+          foreach (var file in files)
+            root.Items.Add(CreateChild(file, false));            
+        }
+
+        private TreeViewItem CreateChild(string path, bool isFoleder)
+        {
+            Element el = new Element(path, isFoleder);
+            TreeViewItem child = new TreeViewItem();
+            child.Header = el;
+            if(isFoleder)
+                child.Items.Add("");
+            return child;
+        }
+    }
 }
